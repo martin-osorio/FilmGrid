efe6a6814553a1de4f5b3909948653112498a4d0
Martin Osorio <me@martinosorio.com>
2018-02-12 12:33:41 -0600

    Creation commit

---
+++ app/src/main/java/com/example/martinosorio/filmgrid/FilmGridActivity.java
@@ -0,0 +1,13 @@
+package com.example.martinosorio.filmgrid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilmGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.film_grid_activity_layout);
+    }
+}
---
+++ app/src/main/java/com/example/martinosorio/filmgrid/FilmGridActivityViewModel.java
@@ -0,0 +1,8 @@
+package com.example.martinosorio.filmgrid;
+
+/**
+ * Created by Martin on 2/12/2018.
+ */
+
+public class FilmGridActivityViewModel {
+}
