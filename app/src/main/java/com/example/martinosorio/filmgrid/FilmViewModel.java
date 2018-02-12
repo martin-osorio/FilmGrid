a062cb517860b911c09c7559912c2ddae5eb4b64
Martin Osorio <me@martinosorio.com>
2018-02-12 16:39:17 -0600

    Add cardview

--- app/src/main/java/com/example/martinosorio/filmgrid/FilmViewModel.java
+++ app/src/main/java/com/example/martinosorio/filmgrid/FilmViewModel.java
@@ -1,6 +1,7 @@
 package com.example.martinosorio.filmgrid;
 
 import android.databinding.Bindable;
+import android.databinding.Observable;
 
 import com.example.martinosorio.filmgrid.model.Film;
 
@@ -8,7 +9,7 @@ import com.example.martinosorio.filmgrid.model.Film;
  * Created by Martin on 2/12/2018.
  */
 
-public class FilmViewModel {
+public class FilmViewModel implements Observable {
     private FilmViewHolder holder;
     private Film film;
     private String title;
@@ -18,7 +19,7 @@ public class FilmViewModel {
         this.holder = holder;
         this.film = film;
         this.title = film.getTitle();
-        this.url = film.getImages().getImage().get(0).getSrc();//TODO
+        //this.url = film.getImages().getImage().get(0).getSrc();//TODO
 
         new ImageDownloader(holder.getImageView()).execute("https://www.google.com/images/srpr/logo11w.png");//TODO
     }
@@ -30,5 +31,15 @@ public class FilmViewModel {
 
     public String getUrl() {
         return url;
+    }
+
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback onPropertyChangedCallback) {
+
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback onPropertyChangedCallback) {
+
     }
 }
