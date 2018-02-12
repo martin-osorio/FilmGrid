c9c0a0a5c9961c489ce4445c463f1d56cda2a3b8
Martin Osorio <me@martinosorio.com>
2018-02-12 13:54:01 -0600

    Add progress bar and recycler view, add databinding for visibility.

--- app/src/main/java/com/example/martinosorio/filmgrid/FilmGridActivityViewModel.java
+++ app/src/main/java/com/example/martinosorio/filmgrid/FilmGridActivityViewModel.java
@@ -1,17 +1,61 @@
 package com.example.martinosorio.filmgrid;
 
+import android.databinding.Bindable;
+import android.databinding.Observable;
+import android.view.View;
+
 /**
  * Created by Martin on 2/12/2018.
  */
 
-public class FilmGridActivityViewModel {
+public class FilmGridActivityViewModel implements Observable {
+    boolean progressVisibility;
+    boolean recyclerViewVisibility;
 
     public FilmGridActivityViewModel() {
+        setProgressVisibility(true);
+        setRecyclerViewVisibility(false);
         startController();
     }
 
     private void startController(){
         SnagfilmsController controller = new SnagfilmsController();
         controller.start();
+    }
+
+    public boolean isProgressVisibility() {
+        return progressVisibility;
+    }
+
+    @Bindable
+    public int getProgressVisibility(){
+        return isProgressVisibility() ? View.VISIBLE : View.GONE;
+    }
+
+    public void setProgressVisibility(boolean progressVisibility) {
+        this.progressVisibility = progressVisibility;
+    }
+
+    public boolean isRecyclerViewVisibility() {
+        return recyclerViewVisibility;
+    }
+
+    @Bindable
+    public int getRecyclerViewVisibility(){
+        return isRecyclerViewVisibility() ? View.VISIBLE : View.GONE;
+    }
+
+    public void setRecyclerViewVisibility(boolean recyclerViewVisibility) {
+        this.recyclerViewVisibility = recyclerViewVisibility;
+    }
+
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback onPropertyChangedCallback) {
+        //TODO
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback onPropertyChangedCallback) {
+        //TODO
     }
 }
--- app/src/main/java/com/example/martinosorio/filmgrid/SnagfilmsController.java
+++ app/src/main/java/com/example/martinosorio/filmgrid/SnagfilmsController.java
@@ -1,7 +1,6 @@
 package com.example.martinosorio.filmgrid;
 
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import com.example.martinosorio.filmgrid.model.Films;
 import com.google.gson.Gson;
@@ -18,12 +17,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class SnagfilmsController implements Callback<Films> {
-    public static final String TAG = "moltag";
-
     static final String BASE_URL = "http://www.snagfilms.com/apis/";
 
     public void start() {
-        Log.d(TAG, "start: ");
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
@@ -35,28 +31,22 @@ public class SnagfilmsController implements Callback<Films> {
 
         SnagfilmsAPI snagfilmsAPI = retrofit.create(SnagfilmsAPI.class);
 
-        Log.d(TAG, "start: getting films");
         Call<Films> call = snagfilmsAPI.getFilms();
         call.enqueue(this);
-        Log.d(TAG, "start: Queued");
 
     }
 
     @Override
     public void onResponse(@NonNull Call<Films> call, @NonNull Response<Films> response) {
-        Log.d(TAG, "onResponse: ");
         if(response.isSuccessful()) {
             Films films = response.body();
-            Log.d(TAG, "onResponse: success");
         } else {
-            Log.d(TAG, "onResponse: not successful");
             System.out.println(response.errorBody());
         }
     }
 
     @Override
     public void onFailure(@NonNull Call<Films> call, @NonNull Throwable t) {
-        Log.d(TAG, "onFailure: ");
         t.printStackTrace();
     }
 }
