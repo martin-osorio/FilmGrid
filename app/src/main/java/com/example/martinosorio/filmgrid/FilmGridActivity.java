9f477fc4fe174ab4ecd01fe338a214e1c54f8ada
Martin Osorio <me@martinosorio.com>
2018-02-12 13:39:46 -0600

    Implement databinding

--- app/src/main/java/com/example/martinosorio/filmgrid/FilmGridActivity.java
+++ app/src/main/java/com/example/martinosorio/filmgrid/FilmGridActivity.java
@@ -1,13 +1,18 @@
 package com.example.martinosorio.filmgrid;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.martinosorio.filmgrid.databinding.FilmGridActivityLayoutBinding;
 
 public class FilmGridActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.film_grid_activity_layout);
+        FilmGridActivityLayoutBinding binding = DataBindingUtil.setContentView(this, R.layout.film_grid_activity_layout);
+        FilmGridActivityViewModel viewModel = new FilmGridActivityViewModel();
+        binding.setViewModel(viewModel);
     }
 }
